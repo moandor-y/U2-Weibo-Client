3b1968450d26be01b8a6e395f68f606c82f3908d
Moandor <moandor.y@gmail.com>
2014-03-17 21:29:36 +0800

    修复：浏览图片闪退

---
+++ src/gov/moandor/androidweibo/widget/ViewPagerFixed.java
@@ -0,0 +1,38 @@
+package gov.moandor.androidweibo.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import gov.moandor.androidweibo.util.Logger;
+
+public class ViewPagerFixed extends ViewPager {
+    public ViewPagerFixed(Context context) {
+        super(context);
+    }
+    
+    public ViewPagerFixed(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            Logger.logExcpetion(e);
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            Logger.logExcpetion(e);
+        }
+        return false;
+    }
+}
