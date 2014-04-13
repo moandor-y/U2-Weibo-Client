aa353b69f00771572fc4f66b02df800ea81cf746
Moandor <moandor.y@gmail.com>
2014-04-13 17:47:10 +0800

    clean up

--- src/gov/moandor/androidweibo/util/HttpUtils.java
+++ src/gov/moandor/androidweibo/util/HttpUtils.java
@@ -74,48 +74,7 @@ public class HttpUtils {
     public static enum Method {
         POST, GET
     }
-    /*
-    public static final class UrlHelper {
-        public static final String APPKEY = "1578201915";
-        public static final String AUTH_REDIRECT = "https://api.weibo.com/oauth2/default.html";
-        
-        public static final String OAUTH2_AUTHORIZE = "https://api.weibo.com/oauth2/authorize";
-        public static final String OAUTH2_ACCESS_TOKEN = "https://api.weibo.com/oauth2/access_token";
-        public static final String STATUSES_FRIENDS_TIMELINE = "https://api.weibo.com/2/statuses/friends_timeline.json";
-        public static final String STATUSES_BILATERAL_TIMELINE =
-                "https://api.weibo.com/2/statuses/bilateral_timeline.json";
-        public static final String STATUSES_MENTIONS = "https://api.weibo.com/2/statuses/mentions.json";
-        public static final String STATUSES_REPOST_TIMELINE = "https://api.weibo.com/2/statuses/repost_timeline.json";
-        public static final String STATUSES_SHOW = "https://api.weibo.com/2/statuses/show.json";
-        public static final String STATUSES_USER_TIMELINE = "https://api.weibo.com/2/statuses/user_timeline.json";
-        public static final String STATUSES_UPDATE = "https://api.weibo.com/2/statuses/update.json";
-        public static final String STATUSES_REPOST = "https://api.weibo.com/2/statuses/repost.json";
-        public static final String STATUSES_UPLOAD = "https://upload.api.weibo.com/2/statuses/upload.json";
-        public static final String STATUSES_DESTROY = "https://api.weibo.com/2/statuses/destroy.json";
-        public static final String ACCOUNT_GET_UID = "https://api.weibo.com/2/account/get_uid.json";
-        public static final String USERS_SHOW = "https://api.weibo.com/2/users/show.json";
-        public static final String COMMENTS_TO_ME = "https://api.weibo.com/2/comments/to_me.json";
-        public static final String COMMENTS_BY_ME = "https://api.weibo.com/2/comments/by_me.json";
-        public static final String COMMENTS_MENTIONS = "https://api.weibo.com/2/comments/mentions.json";
-        public static final String COMMENTS_SHOW = "https://api.weibo.com/2/comments/show.json";
-        public static final String COMMENTS_CREATE = "https://api.weibo.com/2/comments/create.json";
-        public static final String COMMENTS_REPLY = "https://api.weibo.com/2/comments/reply.json";
-        public static final String COMMENTS_DESTROY = "https://api.weibo.com/2/comments/destroy.json";
-        public static final String FRIENDSHIPS_FRIENDS = "https://api.weibo.com/2/friendships/friends.json";
-        public static final String FRIENDSHIPS_FOLLOWERS = "https://api.weibo.com/2/friendships/followers.json";
-        public static final String FRIENDSHIPS_CREATE = "https://api.weibo.com/2/friendships/create.json";
-        public static final String FRIENDSHIPS_DESTROY = "https://api.weibo.com/2/friendships/destroy.json";
-        public static final String REMIND_UNREAD_COUNT = "https://rm.api.weibo.com/2/remind/unread_count.json";
-        public static final String REMIND_SET_COUNT = "https://rm.api.weibo.com/2/remind/set_count.json";
-        public static final String SEARCH_TOPICS = "https://api.weibo.com/2/search/topics.json";
-        public static final String SEARCH_SUGGESTIONS_AT_USERS =
-                "https://api.weibo.com/2/search/suggestions/at_users.json";
-        public static final String FAVORITES = "https://api.weibo.com/2/favorites.json";
-        public static final String FAVORITES_CREATE = "https://api.weibo.com/2/favorites/create.json";
-        public static final String FAVORITES_DESTROY = "https://api.weibo.com/2/favorites/destroy.json";
-        public static final String DIRECT_MESSAGES_USER_LIST = "https://api.weibo.com/2/direct_messages/user_list.json";
-    }
-    */
+    
     private static Proxy getProxy() {
         String proxyHost = System.getProperty("http.proxyHost");
         String proxyPort = System.getProperty("http.proxyPort");
