6183f129180009b8798050dee6a07d99b76e2d48
Moandor <moandor.y@gmail.com>
2014-05-16 13:28:41 +0800

    image viewer rotate

--- src/gov/moandor/androidweibo/util/ActivityUtils.java
+++ src/gov/moandor/androidweibo/util/ActivityUtils.java
@@ -2,6 +2,7 @@ package gov.moandor.androidweibo.util;
 import android.content.Intent;
 import gov.moandor.androidweibo.bean.WeiboStatus;
 import gov.moandor.androidweibo.activity.WeiboActivity;
+import gov.moandor.androidweibo.activity.MainActivity;
 
 public class ActivityUtils {
 	public static Intent weiboActivity(WeiboStatus status) {
